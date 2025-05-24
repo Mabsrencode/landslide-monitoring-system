95a564549d1e8f4c6445d5ee843eba2248492255
Reniel George Mababa <mabsrencode@gmail.com>
2025-05-25 04:10:41 +0800

    FEAT: React pdf setup.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: false,
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        canvas: "./empty-module.ts",
+      },
+    },
+  },
 };
 
 export default nextConfig;
