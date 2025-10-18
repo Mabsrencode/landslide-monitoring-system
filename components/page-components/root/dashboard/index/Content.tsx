712bababa0e9faaac7355e6f6f066b3f891a0b71
Reniel George Mababa <mabsrencode@gmail.com>
2025-10-18 14:00:52 +0800

    SELECT

--- components/page-components/root/dashboard/index/Content.tsx
+++ components/page-components/root/dashboard/index/Content.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import {
   LineChart,
@@ -30,14 +30,16 @@ const Content = () => {
 
   const [sensorData, setSensorData] = useState<RealtimeSensorData>(null);
   const [isToggling, setIsToggling] = useState(false);
+  const [filterRange, setFilterRange] = useState<"today" | "15days" | "30days">(
+    "today"
+  );
+
   useEffect(() => {
     const dataRef = ref(database, "sensors/");
-
     const unsubscribe = onValue(dataRef, (snapshot) => {
       const data = snapshot.val();
       setSensorData(data);
     });
-
     return () => unsubscribe();
   }, []);
 
@@ -76,6 +78,7 @@ const Content = () => {
       staleTime: 5 * 60 * 1000,
       enabled: !!isAdmin,
     });
+
   const { data: incidentData, isPending: isLoadingIncidentData } = useQuery<
     LogsProps[] | null
   >({
@@ -84,6 +87,7 @@ const Content = () => {
     staleTime: 5 * 60 * 1000,
     enabled: !!isAdmin,
   });
+
   const { data: noOfUsers, isFetching: isLoadingNoOfUser } =
     useQuery<UserListResponse>({
       queryKey: ["users"],
@@ -98,18 +102,44 @@ const Content = () => {
     staleTime: 5 * 60 * 1000,
   });
 
-  const chartData: ChartDataPoint[] =
-    sensorHistoryData?.map((item) => ({
-      time: new Date(item.createdAt).toLocaleTimeString(),
-      moisture: item.moisture.value,
-      rain: item.rain.value,
-      vibration: item.vibration.value,
-    })) || [];
+  const chartData: ChartDataPoint[] = useMemo(() => {
+    if (!sensorHistoryData) return [];
+
+    const now = new Date();
+    const filtered = sensorHistoryData.filter((item) => {
+      if (!item.createdAt) return false;
+      const createdAt = new Date(item.createdAt);
+
+      if (filterRange === "today") {
+        return createdAt.toDateString() === now.toDateString();
+      }
+
+      const diffDays =
+        (now.getTime() - createdAt.getTime()) / (1000 * 60 * 60 * 24);
+      if (filterRange === "15days") return diffDays <= 15;
+      if (filterRange === "30days") return diffDays <= 30;
+
+      return true;
+    });
+
+    filtered.sort(
+      (a, b) =>
+        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+
+    return filtered.map((item) => ({
+      time: new Date(item.createdAt).toLocaleString(),
+      moisture: item.moisture?.value ?? 0,
+      rain: item.rain?.value ?? 0,
+      vibration: item.vibration?.value ?? 0,
+    }));
+  }, [sensorHistoryData, filterRange]);
 
   const numberOfVerifiedResidents =
     usersData &&
     usersData.data &&
     usersData.data.filter((e) => e.status === "active");
+
   let statusColor: string;
   switch (sensorData?.warningLevel.color) {
     case "RED":
@@ -127,7 +157,9 @@ const Content = () => {
     default:
       statusColor = "bg-green-500";
   }
+
   if (!user) return <MainLoader />;
+
   return (
     <Section>
       <div className="flex items-center gap-4 w-full justify-between">
@@ -157,152 +189,174 @@ const Content = () => {
         </div>
       </div>
       {isAdmin && (
-        <div className="p-4 border border-black/20 rounded mt-6">
-          <ul className="mt-4 space-y-1 flex flex-col md:flex-row gap-2 w-full justify-center text-sm text-center">
-            <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
-              <div>
-                {isLoadingUsersData ? (
-                  <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
-                ) : (
-                  <div className="text-4xl my-4">
-                    {numberOfVerifiedResidents?.length}
-                  </div>
-                )}
-                <h3 className="text-2xl manrope">Verified Residents</h3>
-              </div>
-            </li>
-            <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
-              <div>
-                {isLoadingIncidentData ? (
-                  <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
-                ) : (
-                  <div className="text-4xl my-4">{incidentData?.length}</div>
-                )}
-                <h3 className="text-2xl manrope">Alerts</h3>
-              </div>
-            </li>
-            <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
-              <div>
-                {isLoadingNoOfUser ? (
-                  <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
-                ) : (
-                  <div className="text-4xl my-4">{noOfUsers?.data?.length}</div>
-                )}
-                <h3 className="text-2xl manrope">No of Users</h3>
-              </div>
-            </li>
-          </ul>
-        </div>
-      )}
-      {!isAdmin && (
-        <div className="p-4 border border-black/20 rounded mt-6">
-          <h3 className="font-semibold text-2xl manrope text-center capitalize">
-            current readings
-          </h3>
-          <ul className="mt-4 space-y-1 flex flex-col md:flex-row gap-2 w-full justify-center text-sm text-center">
-            <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
-              <div className="text-7xl">💧</div>
-              <div className="text-4xl my-4">
-                {sensorData
-                  ? `${Math.round((sensorData.moisture.value / 4095) * 100)}%`
-                  : "N/A"}
-              </div>
-              <h3 className="text-2xl manrope">Moisture</h3>
-            </li>
-
-            <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
-              <div className="text-7xl">🌧️</div>
-              <div className="text-4xl my-4">
-                {sensorData
-                  ? `${Math.round((sensorData.rain.value / 4095) * 100)}%`
-                  : "N/A"}
-              </div>
-              <h3 className="text-2xl manrope">Rain</h3>
-            </li>
-
-            <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
-              <div className="text-7xl">♒︎</div>
-              <div className="text-4xl my-4">
-                {sensorData
-                  ? `${Math.round((sensorData.vibration.value / 4095) * 100)}%`
-                  : "N/A"}
-              </div>
-              <h3 className="text-2xl manrope">Soil Vibration</h3>
-            </li>
-          </ul>
-        </div>
-      )}
-
-      {isAdmin && (
-        <div className="mt-4 space-y-6 p-4 border border-black/20 rounded">
-          <h3 className="font-semibold text-4xl mb-2 text-center capitalize">
-            Current readings
-          </h3>
-          <div className="flex items-center justify-center">
-            <div className="grid xl:grid-cols-2 gap-4">
-              <LineChart
-                width={chartWidth}
-                height={250}
-                data={chartData}
-                className="border border-black/20 w-full p-2 rounded"
+        <>
+          <div className="p-4 border border-black/20 rounded mt-6">
+            <ul className="mt-4 space-y-1 flex flex-col md:flex-row gap-2 w-full justify-center text-sm text-center">
+              <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
+                <div>
+                  {isLoadingUsersData ? (
+                    <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
+                  ) : (
+                    <div className="text-4xl my-4">
+                      {numberOfVerifiedResidents?.length}
+                    </div>
+                  )}
+                  <h3 className="text-2xl manrope">Verified Residents</h3>
+                </div>
+              </li>
+              <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
+                <div>
+                  {isLoadingIncidentData ? (
+                    <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
+                  ) : (
+                    <div className="text-4xl my-4">{incidentData?.length}</div>
+                  )}
+                  <h3 className="text-2xl manrope">Alerts</h3>
+                </div>
+              </li>
+              <li className="p-4 rounded bg-secondary text-white w-[300px] min-h-fit flex items-center justify-center">
+                <div>
+                  {isLoadingNoOfUser ? (
+                    <div className="w-[50px] h-[50px] border-3 mx-auto border-white border-t-transparent rounded-full animate-spin mb-4"></div>
+                  ) : (
+                    <div className="text-4xl my-4">
+                      {noOfUsers?.data?.length}
+                    </div>
+                  )}
+                  <h3 className="text-2xl manrope">No of Users</h3>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-4 space-y-6 p-4 border border-black/20 rounded">
+            <div className="flex items-center justify-between mb-4">
+              <h3 className="font-semibold text-4xl manrope capitalize">
+                Current Readings
+              </h3>
+              <select
+                value={filterRange}
+                onChange={(e) =>
+                  setFilterRange(
+                    e.target.value as "today" | "15days" | "30days"
+                  )
+                }
+                className="border border-black/20 rounded p-2 text-sm manrope focus:outline-none"
               >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="moisture"
-                  stroke="#8884d8"
-                  name="Soil Moisture"
-                />
-              </LineChart>
-              <LineChart
-                width={chartWidth}
-                height={250}
-                data={chartData}
-                className="border border-black/20 w-full p-2 rounded"
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="vibration"
-                  stroke="#82ca9d"
-                  name="Vibration"
-                />
-              </LineChart>
-              <LineChart
-                width={chartWidth}
-                height={250}
-                data={chartData}
-                className="border border-black/20 w-full p-2 rounded"
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="rain"
-                  stroke="#ff7300"
-                  name="Rain"
-                />
-              </LineChart>
+                <option value="today">Today</option>
+                <option value="15days">Last 15 Days</option>
+                <option value="30days">Last 30 Days</option>
+              </select>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <div className="grid xl:grid-cols-2 gap-4">
+                <LineChart
+                  width={chartWidth}
+                  height={250}
+                  data={chartData}
+                  className="border border-black/20 w-full p-2 rounded"
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="moisture"
+                    stroke="#8884d8"
+                    name="Soil Moisture"
+                  />
+                </LineChart>
+
+                <LineChart
+                  width={chartWidth}
+                  height={250}
+                  data={chartData}
+                  className="border border-black/20 w-full p-2 rounded"
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="vibration"
+                    stroke="#82ca9d"
+                    name="Vibration"
+                  />
+                </LineChart>
+
+                <LineChart
+                  width={chartWidth}
+                  height={250}
+                  data={chartData}
+                  className="border border-black/20 w-full p-2 rounded"
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="rain"
+                    stroke="#ff7300"
+                    name="Rain"
+                  />
+                </LineChart>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
       {!isAdmin && (
-        <div className="mt-12">
-          <h3 className="manrope text-2xl font-semibold">Recent Alerts</h3>
-          <IncidentTable pagination={false} />
-        </div>
+        <>
+          <div className="p-4 border border-black/20 rounded mt-6">
+            <h3 className="font-semibold text-2xl manrope text-center capitalize">
+              current readings
+            </h3>
+            <ul className="mt-4 space-y-1 flex flex-col md:flex-row gap-2 w-full justify-center text-sm text-center">
+              <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
+                <div className="text-7xl">💧</div>
+                <div className="text-4xl my-4">
+                  {sensorData
+                    ? `${Math.round((sensorData.moisture.value / 4095) * 100)}%`
+                    : "N/A"}
+                </div>
+                <h3 className="text-2xl manrope">Moisture</h3>
+              </li>
+
+              <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
+                <div className="text-7xl">🌧️</div>
+                <div className="text-4xl my-4">
+                  {sensorData
+                    ? `${Math.round((sensorData.rain.value / 4095) * 100)}%`
+                    : "N/A"}
+                </div>
+                <h3 className="text-2xl manrope">Rain</h3>
+              </li>
+
+              <li className="p-4 rounded bg-secondary text-white w-[300px] h-full">
+                <div className="text-7xl">♒︎</div>
+                <div className="text-4xl my-4">
+                  {sensorData
+                    ? `${Math.round(
+                        (sensorData.vibration.value / 4095) * 100
+                      )}%`
+                    : "N/A"}
+                </div>
+                <h3 className="text-2xl manrope">Soil Vibration</h3>
+              </li>
+            </ul>
+          </div>
+
+          <div className="mt-12">
+            <h3 className="manrope text-2xl font-semibold">Recent Alerts</h3>
+            <IncidentTable pagination={false} />
+          </div>
+        </>
       )}
     </Section>
   );
